b5c043c32a53c518f0d78df4928c8e28f321f71a
Daigo Uenuma <go26dev@gmail.com>
2019-05-12 02:14:39 +0900

    募集画面 修正

--- pages/index.vue
+++ pages/index.vue
@@ -70,8 +70,12 @@
                 <v-hover>
                   <v-card
                     slot-scope="{ hover }"
-                    class="mb-5"
-                    :class="`elevation-${hover ? 12 : 2}`"
+                    :class="{
+                      'mb-5': $vuetify.breakpoint.smAndUp,
+                      'mb-4': $vuetify.breakpoint.xsOnly,
+                      'elevation-12': hover && !$vuetify.breakpoint.xsOnly,
+                      'elevation-2': !hover,
+                    }"
                   >
                     <v-card flat :to='"jobs/" + job.jobId' class="clickable">
                       <v-img
@@ -91,11 +95,11 @@
                               'headline': $vuetify.breakpoint.smAndUp,
                               'title': $vuetify.breakpoint.xsOnly,
                             }"
-                            class="textColor font-weight-bold mb-0"
+                            class="textColor font-weight-bold break mb-0"
                           >
                             {{ job.title }}
                           </h3>
-                          <div class="textColor pt-3">{{ job.content }}</div>
+                          <div class="textColor break pt-3">{{ job.content }}</div>
                         </div>
                       </v-card-title>
                     </v-card>
@@ -116,11 +120,11 @@
                               class="textColor font-weight-bold"
                             >
                               {{ job.companyName }}
-                              <span v-show="hover" class="pl-2 caption green--text">企業情報を見る</span>
                             </v-list-tile-title>
-                            <v-list-tile-sub-title v-if="job.rating">
+                            <v-list-tile-sub-title>
                               <v-card-actions class="pa-0">
                                 <v-rating
+                                  v-if="job.rating"
                                   v-model="job.rating.all"
                                   background-color="teal"
                                   color="teal darken-1"
@@ -128,7 +132,8 @@
                                   half-increments
                                   readonly
                                 />
-                                <span class="pl-1 textColor">{{ job.rating.count }}</span>
+                                <span v-if="job.rating" class="pl-1 pr-3 textColor">{{ job.rating.count }}</span>
+                                <span v-show="hover" class="caption green--text">企業情報を見る</span>
                               </v-card-actions>
                             </v-list-tile-sub-title>
                           </v-list-tile-content>
