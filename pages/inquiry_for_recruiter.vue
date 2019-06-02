41b0082964c23338aa704c405592da6b50e97882
Daigo Uenuma <go26dev@gmail.com>
2019-06-03 04:34:49 +0900

    企業用の問い合わせ画面 更新

--- pages/inquiry_for_recruiter.vue
+++ pages/inquiry_for_recruiter.vue
@@ -22,11 +22,156 @@
         Close
       </v-btn>
     </v-snackbar>
-    <v-flex xs12 sm8 md6 offset-sm2 offset-md3>
-      <v-card>
-        <v-toolbar flat color="white">
-          <span class="text-color font-weight-bold subheading">資料請求・お問い合わせ</span>
-        </v-toolbar>
+    <v-flex xs12>
+      <!-- 見出し -->
+      <v-parallax
+        dark
+        :style="{ height: windowHeight + 'px' }"
+        style="
+          background-color: #e0f7fa;
+          background-image: linear-gradient(315deg, #e0f7fa 0%, #009688 73%);
+        "
+      >
+        <v-layout
+          align-center
+          row
+        >
+          <v-flex
+            xs12
+            sm9
+            md7
+            class=" text-xs-center"
+            :class="{
+              'pl-3': $vuetify.breakpoint.xsOnly,
+              'padding-left-sm': $vuetify.breakpoint.smOnly,
+              'padding-left-md': $vuetify.breakpoint.mdOnly,
+              'padding-left-lg': $vuetify.breakpoint.lgOnly,
+            }"
+          >
+            <div class="text-xs-left">
+              <h1 class="headline font-weight-bold mb-3">LightHouse</h1>
+              <h1
+                class="pt-4 font-weight-bold mb-3"
+                :class="{
+                  'eading-font-size-xs': $vuetify.breakpoint.xsOnly,
+                  'heading-font-size-sm': $vuetify.breakpoint.smAndUp,
+                }"
+                style="max-width: 500px;"
+              >
+                <div>
+                  長期インターンで
+                </div>
+                <div>
+                  採用に変化を
+                </div>
+              </h1>
+              <h4 class="pt-4 subheading font-weight-bold" style="max-width: 500px">
+                LightHouse は長期インターンを通して、
+                求人者と求職者をマッチングする
+                採用プラットフォームです。
+                企業の雰囲気や文化、求職者の働き方や性格など
+                実際に働いてみないと分からないことをお互いが知ることで、
+                ミスマッチを減らし、より良い採用が出来るようサポートをします。
+              </h4>
+            </div>
+          </v-flex>
+          <!-- <v-flex xs5>
+            <v-img max-width="400" v-bind:src="require('@/assets/images/aurora.png')"/>
+          </v-flex> -->
+        </v-layout>
+      </v-parallax>
+      <!-- 機能 -->
+      <div class="py-5">
+        <div class="text-xs-center font-weight-bold headline text-color">
+          機能・特徴
+        </div>
+        <v-flex xs12 sm8 offset-sm2 class="pt-2">
+          <v-divider></v-divider>
+        </v-flex>
+        <v-flex xs10 sm8 md6 offset-xs1 offset-sm2 offset-md3 class="pt-4">
+          <div class="py-4 text-color">
+            <div class="font-weight-bold subheading text-color">
+              1. 募集
+            </div>
+            <div class="pt-2">
+              募集を作成して企業や仕事の魅力を伝えましょう。
+            </div>
+          </div>
+          <div class="py-4 text-color">
+            <div class="font-weight-bold subheading text-color">
+              2. スカウト
+            </div>
+            <div class="pt-2">
+              気になる求職者がいたらスカウトをしてアプローチしましょう。
+            </div>
+          </div>
+          <div class="py-4 text-color">
+            <div class="font-weight-bold subheading text-color">
+              3. レビュー
+            </div>
+            <div class="pt-2">
+              インターン後、候補者にレビューをしてもらえます。これにより、候補者から見た
+              企業の良い点や改善すべき点などを知ることが出来ます。また、良いレビューをもらえると
+              検索で上位に表示されるため、求職者に認知されやすくなります。
+            </div>
+          </div>
+          <div class="py-4 text-color">
+            <div class="font-weight-bold subheading text-color">
+              4. パス
+            </div>
+            <div class="pt-2">
+              インターン後、企業が採用したいと思う候補者にパスを渡すことが出来ます。
+              パスを発行することで、候補者に入社や内定取得の権利を与えることができます。
+              これにより、優秀な求職者の採用機会を増やすことが出来ます。
+            </div>
+          </div>
+        </v-flex>
+      </div>
+      <!-- 料金 -->
+      <div class="py-5">
+        <div class="pb-3 text-xs-center font-weight-bold headline text-color">
+          料金プラン
+        </div>
+        <v-flex xs12 sm8 md6 offset-sm2 offset-md3>
+          <v-card class="text-xs-center ma-2">
+            <div class="py-3 font-weight-bold text-color">
+              成功報酬型
+            </div>
+            <v-divider class="mx-3"></v-divider>
+            <div class="py-4 text-color">
+              <div>
+                採用人数に応じて
+              </div>
+              <div>
+                費用が発生します
+              </div>
+            </div>
+            <v-divider class="mx-3"></v-divider>
+            <div class="py-4 text-color">
+              <div>
+                募集作成やスカウトなど
+              </div>
+              <div>
+                すべての機能を制限なく使用できます
+              </div>
+            </div>
+            <v-divider class="mx-3"></v-divider>
+            <div class="py-4 text-color">
+              掲載期間は6ヶ月（自動更新）
+            </div>
+          </v-card>
+          <div class="pt-2 light-text-color text-xs-right">
+            ※ 料金プランは今後増える予定です。
+          </div>
+        </v-flex>
+      </div>
+    </v-flex>
+    <v-flex xs12 sm8 md6 offset-sm2 offset-md3 class="py-5">
+      <!-- フォーム -->
+      <div class="pb-3 text-xs-center font-weight-bold headline text-color">
+        資料請求・お問い合わせ
+      </div>
+      <v-card class="ma-2">
         <v-flex
           xs12
           class="text-xs-center"
@@ -135,6 +280,7 @@ import { mapActions, mapState, mapGetters } from 'vuex'
 
 export default {
   data: () => ({
+    windowHeight: 0,
     snackbar: false,
     snackbarText: '',
     valid: true,
@@ -187,6 +333,15 @@ export default {
       return this.$vuetify.breakpoint.name
     },
   },
+  mounted() {
+    let toolbarHeight
+    if (this.breakpoint == 'xs' || this.breakpoint == 'sm') {
+      toolbarHeight = 48
+    } else {
+      toolbarHeight = 64
+    }
+    this.windowHeight = window.innerHeight - toolbarHeight
+  },
   methods: {
     sendButtonClicked() {
       let type
@@ -217,3 +372,20 @@ export default {
   }
 }
 </script>
+<style>
+.padding-left-lg {
+  padding-left: 120px;
+}
+.padding-left-md {
+  padding-left: 100px;
+}
+.padding-left-sm {
+  padding-left: 80px;
+}
+.heading-font-size-sm {
+  font-size: 40px;
+}
+.heading-font-size-xs {
+  font-size: 30px;
+}
+</style>
